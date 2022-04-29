bf3960f7fce0e4d41cd7b153b9220d6b37bca7e9
tamiaquiyara <93910971+tamiaquiyara@users.noreply.github.com>
2022-04-29 15:04:30 +0200

    Update calculate-bill.js

--- js/calculate-bill.js
+++ js/calculate-bill.js
@@ -1,5 +1,5 @@
 //get a reference to the calculate button
-
+const billStringElement = document.querySelector(".billString");
 //get a reference to the billTotal element
 
 //get a reference to the billString
@@ -9,5 +9,27 @@
 //  * loop over all the entries in the the resulting list
 //  * check if it is a call or an sms and add the right amount to the overall total
 //  * once done looping over all the entries - display the total onto the screen in the billTotal element
-
+function calculateBtnClicked(){
+    // get the string entered in the textArea
+    var billString = billStringElement.value;
+    //split the string
+    var billItems = billString.split(",");
+    // a variable for the total phone bill.
+    var billTotal = 0;
+    //loop over all the bill items
+    for (var i=0;i<billItems.length;i++){
+        var billItem = billItems[i].trim();
+        if (billItem === "call"){
+            billTotal += 2.75;
+        }
+        else if (billItem === "sms"){
+            billTotal += 0.75;
+        }
+    }
+    
+    //round to two decimals
+    var roundedBillTotal = billTotal.toFixed(2);
+    billTotalElement.innerHTML = roundedBillTotal;
+}
 //link the function to a click event on the calculate button
+calculateBtn.addEventListener('click', calculateBtnClicked);
